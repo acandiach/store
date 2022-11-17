38dc94cacda5f139a172ba93ccd7d55acec87316
acandia <abelcandiach@gmail.com>
2022-11-17 17:58:02 -0300

    Agregando gema pg para produccion y tablas de producto y marca

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,20 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+require 'csv'
+
+puts 'Importing brands 5...'
+CSV.foreach(Rails.root.join('db/seeds/csv/brands.csv'), headers: true) do |row|
+  Brand.create! do |brand|
+    brand.id = row[0]
+    brand.name = row[1]
+  end
+end
+
+
+#puts 'Importing products 10...'
+#10.times do
+  #Product = Product.new(
+    #name: Faker::Commerce.product_name,
+    #description: Faker::Lorem.paragraph,
+    #brand_id: rand(1..5),
+  #)
+  #product.save!
+#end
