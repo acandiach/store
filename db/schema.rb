d06511aed8e74a821be4960305eabe7067b7a900
acandia <abelcandiach@gmail.com>
2022-11-22 22:45:03 -0300

    corrigiendo error de referencia

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_22_004847) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_23_012524) do
   create_table "brands", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
@@ -23,7 +23,9 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_22_004847) do
     t.integer "brand_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id", null: false
     t.index ["brand_id"], name: "index_products_on_brand_id"
+    t.index ["user_id"], name: "index_products_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -39,4 +41,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_22_004847) do
   end
 
   add_foreign_key "products", "brands"
+  add_foreign_key "products", "users"
 end
